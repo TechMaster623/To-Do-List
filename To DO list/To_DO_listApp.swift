c75ef5ab0ab1f50b342210bcde001df48281b752
Faiz Ali <fali@bsd220.org>
2023-01-23 09:30:16 -0600

    Initial Commit

---
+++ To DO list/To_DO_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_DO_listApp.swift
+//  To DO list
+//
+//  Created by Faiz Ali on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct To_DO_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
