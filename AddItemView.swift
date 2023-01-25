073d98fa0a3b0765506f2adf9e34c8ea910c3805
Faiz Ali <fali@bsd220.org>
2023-01-25 09:48:49 -0600

    Created AddItems View

---
+++ AddItemView.swift
@@ -0,0 +1,34 @@
+//
+//  AddItemView.swift
+//  To DO list
+//
+//  Created by Faiz Ali on 1/25/23.
+//
+
+import SwiftUI
+
+struct AddItemView: View {
+    @ObservedObject var toDoList: ToDoList
+    @State private var priority = ""
+    @State private var description = ""
+    @State private var dueDate = Date()
+    @Environment(\.presentationMode) var presentationMode
+    static let priorities = ["High", "Medium", "Low"]
+    var body: some View {
+        NavigationView {
+            Form {
+                Picker("Priority", selection: $priority) {
+                    ForEach(Self.priorities, id: \.self) {
+                        priority in Text(priority)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct AddItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddItemView(toDoList: ToDoList())
+    }
+}
